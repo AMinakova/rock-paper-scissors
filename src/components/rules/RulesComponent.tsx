e64a27ad685345cc1cf5904c53b7e3a61cd77cf1
AMinakova <anna.minakova.de@gmail.com>
2024-04-11 14:28:44 +0200

    added image preload for rules component

--- src/components/rules/RulesComponent.tsx
+++ src/components/rules/RulesComponent.tsx
@@ -1,8 +1,13 @@
 import image from "../../assets/image-rules.svg";
 import close from "../../assets/icon-close.svg";
 import styles from "./Rules.module.css";
+import { useEffect } from "react";
 
 export function RulesComponent(props: { closeModal: () => void }) {
+  useEffect(() => {
+    const img = new Image();
+    img.src = image;
+  }, []);
   return (
     <div className={styles.modalContainer}>
       <div className={styles.modalHeader}>
